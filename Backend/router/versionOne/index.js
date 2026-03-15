154c12e682930a5e64a0c080ac9832ba7d203359
omid2007hope <omid85hope@gmail.com>
2026-03-15 13:38:37 +0100

    Basic Routes in backend for header

--- Backend/router/versionOne/index.js
+++ Backend/router/versionOne/index.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const headerController = require("../../controller/versionOne/Header");
+
 const router = express.Router();
 
 router.get("/server", (_req, res) => {
   res.status(200).send("server is running");
 });
+
+router.get("/health", (_req, res) => {
+  res.status(200).json({
+    status: "OK",
+    service: "Never Forgotten backend",
+  });
+});
+
+router
+  .route("/headers")
+  .get(headerController.getHeaders)
+  .post(headerController.createHeaders);
+
+router
+  .route("/headers/:id")
+  .patch(headerController.updateHeader)
+  .delete(headerController.deleteHeader);
 
 module.exports = router;
